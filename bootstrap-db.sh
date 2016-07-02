40e89cdbc4281c5cbbdfe4ad5ea87a8b75b9f971
AGhost-7 <jonathan.boudreau.92@gmail.com>
2016-07-02 15:11:40 -0400

    woops

--- bootstrap-db.sh
+++ bootstrap-db.sh
@@ -1,4 +1,10 @@
+sudo service postgresql start
+
+su postgres <<DBINIT
 createdb node_shop
 psql node_shop -f sql/tables.sql
 psql node_shop -f sql/views.sql
 psql node_shop -f sql/data.sql
+DBINIT
+
+sudo service postgresql stop
